c6c2f8dfe23a6d7481353bc8009dd6e817c227e8
laisrmacedo <laisrodriguesmacedo@gmail.com>
2025-03-23 00:40:03 +0100

    feat: options with their explanations and buttons to continue or cancel group creation

--- src/app/(tabs)/groups/conditionPage/index.tsx
+++ src/app/(tabs)/groups/conditionPage/index.tsx
@@ -1,28 +1,140 @@
-import { Text, View, Pressable } from 'react-native'
+import { Text, View, Pressable, TouchableOpacity, ScrollView } from 'react-native'
 import { styles } from "./styles";
 import { useState } from 'react';
+import TeamSpirit from '../../../../assets/images/teamSpirit.svg'
+import { theme } from '@/src/theme'
+import { ButtonCustomizer } from "@/src/components/ButtonCustomizer"; import { styles as globalStyles } from "@/src/app/styles";
+import { router } from 'expo-router'
 
 export default function ConditionPage() {
-  const [isChecked, setIsChecked] = useState(false)
+  const [selectedOption, setSelectedOption] = useState<'equal' | 'value' | 'percentage' | 'quota' | null>(null)
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Como será feita a divisão?</Text>
+    <ScrollView style={styles.container}>
+      <View style={{ flex: 1, justifyContent: 'space-between', minHeight: '100%'}}>
+      <View>
+        <Text style={styles.title}>Como será feita a divisão?</Text>
 
-      <View style={styles.containerButton}>
-        <Text style={styles.text}>Igualitária</Text>
-        <Pressable
-          style={[
-            styles.checkbox, 
-            isChecked && styles.checked,
-            { borderColor: isChecked ? '#545F71' : '#9BA5B7' },
-          ]}
-          onPress={() => setIsChecked(!isChecked)}
+        <TouchableOpacity
+          style={styles.containerCheckboxArea}
+          onPress={() => setSelectedOption(selectedOption === 'equal' ? null : 'equal')}
         >
-          {isChecked && <Text style={styles.checkmark}>✓</Text>}
-        </Pressable>
+          <View style={styles.containerTitleCheckbox}>
+            <Text style={[styles.secondaryTitle, selectedOption === 'equal' ? { fontFamily: theme.fontFamily.semiBold } : { fontFamily: theme.fontFamily.regular }]}>Igualitária</Text>
+            <Pressable
+              style={[
+                styles.checkbox,
+                selectedOption === 'equal' && styles.checked,
+                { borderColor: selectedOption === 'equal' ? '#545F71' : '#9BA5B7' },
+              ]}
+            >
+              {selectedOption === 'equal' && <Text style={styles.checkmark}>✓</Text>}
+            </Pressable>
+          </View>
+          <View style={selectedOption === 'equal' ? styles.containerImageText : { display: 'none' }}>
+            <TeamSpirit />
+            <Text style={styles.text}>
+              Na divisão iguálitária todos os participantes pagam o mesmo valor, independente do consumo ou contribuição. Ou seja, para despesa de R$100,00 em que 4 pessoas querem dividir, cada um pagaria R$25,00.
+            </Text>
+          </View>
+        </TouchableOpacity>
 
+        <TouchableOpacity
+          style={styles.containerCheckboxArea}
+          onPress={() => setSelectedOption(selectedOption === 'value' ? null : 'value')}
+        >
+          <View style={styles.containerTitleCheckbox}>
+            <Text style={[styles.secondaryTitle, selectedOption === 'value' ? { fontFamily: theme.fontFamily.semiBold } : { fontFamily: theme.fontFamily.regular }]}>Por valor exato</Text>
+            <Pressable
+              style={[
+                styles.checkbox,
+                selectedOption === 'value' && styles.checked,
+                { borderColor: selectedOption === 'value' ? '#545F71' : '#9BA5B7' },
+              ]}
+            >
+              {selectedOption === 'value' && <Text style={styles.checkmark}>✓</Text>}
+            </Pressable>
+          </View>
+          <View style={selectedOption === 'value' ? styles.containerImageText : { display: 'none' }}>
+            <TeamSpirit />
+            <Text style={styles.text}>
+              Cada pessoa paga o valor exato que consumiu. Exemplo: Uma conta de R$100 com 2 pessoas: a pessoa que consumiu R$ 60 paga R$60, e a outra pessoa paga R$40. Ideal para: Quando há itens com valores distintos e a divisão precisa ser precisa. Observação: Pode ser trabalhoso registrar os valores individuais e nem sempre resulta em valores redondos para pagamento.
+            </Text>
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.containerCheckboxArea}
+          onPress={() => setSelectedOption(selectedOption === 'percentage' ? null : 'percentage')}
+        >
+          <View style={styles.containerTitleCheckbox}>
+            <Text style={[styles.secondaryTitle, selectedOption === 'percentage' ? { fontFamily: theme.fontFamily.semiBold } : { fontFamily: theme.fontFamily.regular }]}>Por percentual</Text>
+            <Pressable
+              style={[
+                styles.checkbox,
+                selectedOption === 'percentage' && styles.checked,
+                { borderColor: selectedOption === 'percentage' ? '#545F71' : '#9BA5B7' },
+              ]}
+            >
+              {selectedOption === 'percentage' && <Text style={styles.checkmark}>✓</Text>}
+            </Pressable>
+          </View>
+          <View style={selectedOption === 'percentage' ? styles.containerImageText : { display: 'none' }}>
+            <TeamSpirit />
+            <Text style={styles.text}>
+              Cada pessoa contribui com um percentual pré-definido do total. Exemplo: Uma conta de R$100 com 3 pessoas: Pessoa 1 (30%): R$30, Pessoa 2 (40%): R$40, Pessoa 3 (30%): R$30. Ideal para: Quando as pessoas têm diferentes níveis de renda ou desejam ter pesos diferentes na divisão. Observação: Requer definir as porcentagens de cada um, o que pode gerar discussões.
+            </Text>
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.containerCheckboxArea}
+          onPress={() => setSelectedOption(selectedOption === 'quota' ? null : 'quota')}
+        >
+          <View style={styles.containerTitleCheckbox}>
+            <Text style={[styles.secondaryTitle, selectedOption === 'quota' ? { fontFamily: theme.fontFamily.semiBold } : { fontFamily: theme.fontFamily.regular }]}>Por cotas</Text>
+            <Pressable
+              style={[
+                styles.checkbox,
+                selectedOption === 'quota' && styles.checked,
+                { borderColor: selectedOption === 'quota' ? '#545F71' : '#9BA5B7' },
+              ]}
+            >
+              {selectedOption === 'quota' && <Text style={styles.checkmark}>✓</Text>}
+            </Pressable>
+          </View>
+          <View style={selectedOption === 'quota' ? styles.containerImageText : { display: 'none' }}>
+            <TeamSpirit />
+            <Text style={styles.text}>
+              Cada pessoa paga um valor fixo previamente definido, independente do valor total da conta. Exemplo: Uma cota de R$50 por pessoa para um jantar. Se o jantar custar R$150, todos pagam R$50, dividindo o restante (R$50) igualmente entre quem consumiu algo a mais. Ideal para: Grupos com orçamentos definidos ou quando há custos fixos (aluguel, internet) a serem divididos. Observação: Pode ser necessário ajustar as cotas conforme o valor real das despesas, e nem sempre todos pagam o valor exato que consumiram.
+            </Text>
+          </View>
+        </TouchableOpacity>
       </View>
-    </View>
+
+      <View 
+        style={[{ marginTop: 'auto', paddingTop: 25, flexDirection: "row", gap: 16 }, selectedOption === null ? {paddingBottom: 0} : {paddingBottom: 25}]}
+      >
+        <ButtonCustomizer.Root
+          type="tertiaryHalfWidth"
+          onPress={() => router.push('/groups')}
+        >
+          <ButtonCustomizer.Title
+            title="Cancelar"
+            customStyles={globalStyles.secondaryButtonText}
+          />
+        </ButtonCustomizer.Root>
+        <ButtonCustomizer.Root
+          type="primaryHalfWidth"
+          onPress={() => console.log("continuar")}
+        >
+          <ButtonCustomizer.Title
+            title="Continuar"
+            customStyles={globalStyles.primaryButtonText}
+          />
+        </ButtonCustomizer.Root>
+      </View>
+      </View>
+    </ScrollView>
   )
 }
--- src/app/(tabs)/groups/conditionPage/styles.ts
+++ src/app/(tabs)/groups/conditionPage/styles.ts
@@ -9,10 +9,10 @@ import {
 
 export const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flexGrow: 1,
     backgroundColor: 'white',
     paddingHorizontal: horizontalScale(24),
-    paddingVertical: verticalScale(24),
+    paddingVertical: verticalScale(24)
   },
   title: {
     fontFamily: theme.fontFamily.bold,
@@ -21,19 +21,32 @@ export const styles = StyleSheet.create({
     color: theme.colors.primaryColor,
     paddingVertical: verticalScale(12),
   },
-  containerButton: {
+  containerCheckboxArea: {
+    borderBottomWidth: 1,
+    borderBottomColor: theme.colors.Gray[300],
+  },
+  containerTitleCheckbox: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
     paddingVertical: verticalScale(12),
-    borderBottomWidth: 1,
-    borderBottomColor: theme.colors.Gray[300],
   },
-  text: {
-    fontFamily: theme.fontFamily.semiBold,
+  secondaryTitle: {
     fontSize: rem(16),
     lineHeight: verticalScale(22),
+    color: theme.colors.primaryColor
+  },
+  text: {
+    fontFamily: theme.fontFamily.regular,
+    fontSize: rem(14),
+    lineHeight: verticalScale(19),
     color: theme.colors.primaryColor,
+  },
+  containerImageText: {
+    paddingVertical: verticalScale(12),
+    paddingHorizontal: horizontalScale(8.5),
+    alignItems: 'center',
+    gap: verticalScale(16),
   },
   checkbox: {
     width: 20,
